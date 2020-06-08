8f1b3a8452c3415b6fa98160af94e161ade07897
hossam <hossamameen948@gmail.com>
2020-06-09 01:03:32 +0200

    edit appProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-use App\Notification;
+
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema; //NEW: Import Schema
 use Illuminate\Support\Facades\View;
@@ -16,9 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
      
-        $data['schoolNotifications'] = Notification::where('user_id' , session('school_id'))->where('table' , 'schools')->get() ; 
-        $data['studentNotifications'] =  Notification::where('user_id' , session('student_id'))->where('table' , 'students')->get() ; 
-        View::share('notifications', $data);
+     
        
         Schema::defaultStringLength(191); //NEW: Increase StringLength
     }
